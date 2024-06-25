b2d5d091e615cfc2c2d42a0717def29ed572538b
lmonsalve22 <lmonsalve22@gmail.com>
2024-06-25 17:29:31 -0400

    clase

--- build1.py
+++ build1.py
@@ -8,6 +8,10 @@ import requests
 from io import StringIO
 import pickle
 
+
+
+
+
 state_file_path = 'ultima_descarga.pkl'
 dict_picklet = {"TA_PersonalPlanta_pickle":5332967810,
     "TA_PersonalContratohonorarios_pickle":5088589288,
@@ -26,9 +30,21 @@ def leer_estado_ultima_descarga(state_file):
             return pickle.load(file)
     return 0
 
+class BaseDatos:
+    def init(self, name,namePickle):
+        self.name = name
+        self.file = f"{name}.csv"
+        self.url = f"https://www.cplt.cl/transparencia_activa/datoabierto/archivos/{name}.csv"
+        self.namePickle = namePickle
+        self.sizePickle = leer_estado_ultima_descarga(namePickle)
+
+lista = [BaseDatos("TA_PersonalPlanta","TA_PersonalPlanta_pickle"),
+        BaseDatos("TA_PersonalContratohonorarios","TA_PersonalContratohonorarios_pickle"),
+        BaseDatos("TA_PersonalCodigotrabajo","TA_PersonalCodigotrabajo_pickle"),
+        BaseDatos("TA_PersonalContrata","TA_PersonalContrata_pickle")]
+
 
 if __name__ == '__main__':
-    for i in dict_picklet.keys():
-        aux = leer_estado_ultima_descarga(i)
-        print(i,aux)
+    for i in lista:
+        print(i.url)
     
